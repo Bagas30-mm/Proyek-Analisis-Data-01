08632a73b35a858857d7614e7fb04463769f603f
Bagas30-mm <bagasramadhan@apps.ipb.ac.id>
2025-03-22 12:35:15 +0700

    dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -66,17 +66,9 @@ st.subheader("3. Berapa besar selisih penggunaan sepeda antara hari kerja dan ha
 st.subheader("3. Berapa besar selisih penggunaan sepeda antara hari kerja dan hari libur?")
 
 # Plot dengan color_discrete_map yang benar
-plot_bar_chart(
-    filtered_df,
-    x='workingday_label',
-    y='cnt_daily',
-    color='workingday_label',
-    title='Perbedaan Penggunaan Sepeda Harian Antara Hari Kerja dan Hari Libur',
-    labels={'workingday_label': 'Hari', 'cnt_daily': 'Jumlah Penyewaan'},
-    color_map={'Hari Kerja': 'salmon', 'Hari Libur': 'skyblue'},
-    category_orders={'workingday_label': ['Hari Libur', 'Hari Kerja']}
+plot_bar_chart(filtered_df, 'workingday_label', 'cnt_daily', 'workingday_label', 'Perbedaan Penggunaan Sepeda Harian Antara Hari Kerja dan Hari Libur', {'workingday_label': 'Hari', 'cnt_daily': 'Jumlah Penyewaan'}, color_map={'Hari Kerja': 'salmon', 'Hari Libur': 'skyblue'}, category_orders={'workingday_label': ['Hari Libur', 'Hari Kerja']})
 
-)
+
 st.write('Analisis perbandingan aktivitas penyewaan sepeda menunjukkan bahwa jumlah penyewaan lebih tinggi pada hari kerja dibandingkan hari libur. Grafik menggambarkan dampak positif hari kerja terhadap frekuensi penyewaan, dengan jumlah sewa harian yang lebih dominan. Untuk mengoptimalkan penyewaan, disarankan menyesuaikan penawaran pada hari kerja, seperti memberikan diskon di jam sibuk. Selain itu, pengembangan program khusus di hari libur, misalnya tur rekreasi atau paket promosi, dapat meningkatkan minat pelanggan. Penelitian lebih lanjut terkait faktor penyebab perbedaan penyewaan antara hari kerja dan hari libur direkomendasikan untuk mendapatkan wawasan yang lebih mendalam guna mendukung strategi peningkatan penyewaan sepeda.')
 
 # Menampilkan kesimpulan dari analisis
