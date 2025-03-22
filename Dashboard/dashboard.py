7752f4562b1a5cbba024385c81fcf869fee2d3fa
Bagas30-mm <bagasramadhan@apps.ipb.ac.id>
2025-03-22 12:01:46 +0700

    dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -49,23 +49,41 @@ seasonal_data = filtered_df.groupby(['season_name'])[['casual_daily', 'registere
 plot_bar_chart(seasonal_data, 'season_name', 'casual_daily', 'casual_daily', 'Jumlah Rata-rata Sewa Harian (Casual Users)', {'casual_daily': 'Rata-rata Jumlah Sewa Harian', 'season_name': 'Musim'}, 'reds')
 plot_bar_chart(seasonal_data, 'season_name', 'registered_daily', 'registered_daily', 'Jumlah Rata-rata Sewa Harian (Registered Users)', {'registered_daily': 'Rata-rata Jumlah Sewa Harian', 'season_name': 'Musim'}, 'blues')
 
+st.write("Pada musim gugur, pengguna casual mencatatkan tingkat penyewaan sepeda tertinggi, didukung oleh cuaca yang nyaman serta keindahan lingkungan. Pengguna terdaftar juga menunjukkan jumlah penyewaan yang signifikan pada musim ini. Meskipun terjadi sedikit penurunan pada musim panas dibandingkan dengan musim gugur, musim panas tetap menjadi periode dengan penyewaan tinggi untuk pengguna casual dan cukup tinggi untuk pengguna terdaftar. Selama musim dingin, penyewaan dari kedua kelompok menurun drastis, yang kemungkinan disebabkan oleh cuaca yang kurang mendukung dan kecenderungan untuk melakukan aktivitas indoor. Pada musim semi, tingkat penyewaan berada pada level yang moderat, lebih rendah dibandingkan dengan musim gugur dan musim panas untuk kedua kelompok. Dibandingkan antara kedua kelompok, pengguna casual cenderung lebih aktif menyewa pada musim gugur, sementara pengguna terdaftar mencapai puncak penyewaan mereka pada musim panas. Kedua kelompok mengalami penurunan serupa selama musim dingin, dengan pola penyewaan yang relatif mirip terlihat pada musim semi.")
+
 # Visualization 2: Hourly Activity
 st.subheader("2. Seperti apa perubahan pola aktivitas penyewaan sepeda sepanjang hari?")
 for user_type in ['casual_hourly', 'registered_hourly', 'cnt_hourly']:
     hourly_counts = filtered_df.groupby('hr')[user_type].mean().reset_index()
     color_scale = {'casual_hourly': 'Magenta', 'registered_hourly': 'Teal', 'cnt_hourly': 'Burg'}
     plot_bar_chart(hourly_counts, 'hr', user_type, user_type, f'Distribusi Penyewaan Sepeda berdasarkan Jam dalam Sehari ({user_type.replace("_hourly", "").title()} Users)', {'hr': 'Waktu Penyewaan', user_type: 'Jumlah Penyewaan'}, color_scale[user_type])
-st.write("Pada musim gugur, pengguna casual mencatatkan tingkat penyewaan sepeda tertinggi, didukung oleh cuaca yang nyaman serta keindahan lingkungan. Pengguna terdaftar juga menunjukkan jumlah penyewaan yang signifikan pada musim ini. Meskipun terjadi sedikit penurunan pada musim panas dibandingkan dengan musim gugur, musim panas tetap menjadi periode dengan penyewaan tinggi untuk pengguna casual dan cukup tinggi untuk pengguna terdaftar. Selama musim dingin, penyewaan dari kedua kelompok menurun drastis, yang kemungkinan disebabkan oleh cuaca yang kurang mendukung dan kecenderungan untuk melakukan aktivitas indoor. Pada musim semi, tingkat penyewaan berada pada level yang moderat, lebih rendah dibandingkan dengan musim gugur dan musim panas untuk kedua kelompok. Dibandingkan antara kedua kelompok, pengguna casual cenderung lebih aktif menyewa pada musim gugur, sementara pengguna terdaftar mencapai puncak penyewaan mereka pada musim panas. Kedua kelompok mengalami penurunan serupa selama musim dingin, dengan pola penyewaan yang relatif mirip terlihat pada musim semi.")
+
+st.write('Aktivitas harian penyewaan sepeda oleh pengguna casual mencapai puncaknya pada sore hingga malam hari, terutama sekitar pukul 17.00 hingga 18.00. Pola ini menunjukkan bahwa pengguna casual lebih sering memanfaatkan sepeda untuk kegiatan rekreasi atau aktivitas non-rutin pada waktu tersebut. Di sisi lain, pengguna terdaftar (registered users) memiliki pola yang berbeda, dengan puncak penyewaan terjadi pada dua periode, yaitu pagi sekitar pukul 8.00 dan sore hingga malam sekitar pukul 17.00 hingga 18.00. Hal ini mengindikasikan bahwa pengguna terdaftar lebih sering menggunakan sepeda untuk kebutuhan harian atau perjalanan kerja pada pagi dan sore hari. Secara keseluruhan, jumlah total penyewaan sepeda (count) mencapai puncaknya pada sore hingga malam, yang mencerminkan kombinasi dari pola kedua kelompok. Waktu-waktu tersebut menunjukkan tingginya popularitas penyewaan sepeda secara umum.')
+
 # Visualization 3: Working Day vs Holiday
 st.subheader("3. Berapa besar selisih penggunaan sepeda antara hari kerja dan hari libur?")
 plot_bar_chart(filtered_df, 'workingday_label', 'cnt_daily', 'workingday_label', 'Perbedaan Penggunaan Sepeda Harian Antara Hari Kerja dan Hari Libur', {'workingday_label': 'Hari', 'cnt_daily': 'Jumlah Penyewaan'}, color_map={'Hari Kerja': 'salmon', 'Hari Libur': 'skyblue'}, category_orders={'workingday_label': ['Hari Libur', 'Hari Kerja']})
 
-# Conclusion
+st.write('Analisis perbandingan aktivitas penyewaan sepeda menunjukkan bahwa jumlah penyewaan lebih tinggi pada hari kerja dibandingkan hari libur. Grafik menggambarkan dampak positif hari kerja terhadap frekuensi penyewaan, dengan jumlah sewa harian yang lebih dominan. Untuk mengoptimalkan penyewaan, disarankan menyesuaikan penawaran pada hari kerja, seperti memberikan diskon di jam sibuk. Selain itu, pengembangan program khusus di hari libur, misalnya tur rekreasi atau paket promosi, dapat meningkatkan minat pelanggan. Penelitian lebih lanjut terkait faktor penyebab perbedaan penyewaan antara hari kerja dan hari libur direkomendasikan untuk mendapatkan wawasan yang lebih mendalam guna mendukung strategi peningkatan penyewaan sepeda.')
+
+# Menampilkan kesimpulan dari analisis
 st.header("Kesimpulan")
 st.write("Dari semua hasil analisis berikut adalah kesimpulan keseluruhan dari semua data tersebut")
-st.text('''1. Pengaruh Cuaca Terhadap Aktivitas Penyewaan:
+st.text('''1.Pengaruh Cuaca Terhadap Aktivitas Penyewaan:
+
 - Aktivitas penyewaan mencapai puncaknya pada musim gugur, diduga dipengaruhi oleh kondisi cuaca yang menyenangkan dan pemandangan yang memikat.
 - Walaupun terdapat penurunan, musim panas tetap menjadi salah satu periode dengan jumlah penyewaan yang tinggi.
 - Musim dingin mengalami penurunan drastis dalam aktivitas penyewaan, kemungkinan disebabkan oleh cuaca yang kurang mendukung serta preferensi terhadap aktivitas di dalam ruangan.
 - Musim semi menunjukkan tingkat penyewaan yang cukup stabil, meskipun masih lebih rendah dibandingkan dengan musim gugur dan musim panas.''')
+
+st.text('''2. Pola Aktivitas Penyewaan Harian:
+
+- Pengguna casual lebih sering menyewa sepeda pada sore hingga malam, mencerminkan kecenderungan mereka untuk melakukan aktivitas rekreasi di luar jam kerja.  
+- Pengguna terdaftar memiliki pola aktivitas yang berbeda, dengan penyewaan tertinggi pada pagi hari serta sore hingga malam, yang mengindikasikan penggunaan sepeda untuk perjalanan kerja atau kebutuhan harian.  
+- Secara keseluruhan, jumlah penyewaan mencapai puncaknya pada sore hingga malam hari.''')
+
+st.text('''3. Perbedaan Penggunaan Sepeda antara Hari Kerja dan Hari Libur:
+- Aktivitas penyewaan sepeda cenderung lebih tinggi pada hari kerja dibandingkan hari libur.  
+- Hari kerja memberikan kontribusi positif terhadap penggunaan sepeda, yang mungkin dipengaruhi oleh kebutuhan komutasi dan rutinitas harian.''')
+
 st.caption('Copyright © Bagas Rizky R')
